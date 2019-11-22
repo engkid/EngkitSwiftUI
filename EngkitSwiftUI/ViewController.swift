1577dd2aed30b74469b5e45e45724d0db912fe13
Engkit Satia Riswara <wb-engkitsat462664@antfin.com>
2019-11-22 18:33:59 +0700

    Initial Commit

---
+++ EngkitSwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EngkitSwiftUI
+//
+//  Created by Engkit Satia Riswara on 22/11/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
